ed92e1e10e404a5a8f9283b56e4d0ec546637cc7
John Gettel <jwgettel@gmail.com>
2020-04-16 17:41:34 -0400

    add data fetch

--- data_fetch.py
+++ data_fetch.py
@@ -1,10 +1,41 @@
-import pandas_dataread.data as dr
+import pandas_datareader.data as dr
 import pandas as pd
+from datetime import datetime, date
 
 class DataFetch:
     def __init__(self, engine):
         self.engine = engine
+        self.datasource = 'yahoo'
+        self.start = '2000-01-01'
+        self.end = datetime.now().strftime('%Y-%m-%d')
 
-    def get_symbols(self):
-        
+    def get_equities(self):
+        ticker_query = 'SELECT symbol FROM symbols WHERE type <> "FUT"'
+        symbols = pd.read_sql_query(ticker_query, self.engine)
+        for symbol in symbols['symbol']:
+            start_date_query = 'SELECT MAX(Date) AS Date FROM data WHERE Symbol="{}"'.format(symbol)
+            start_date = pd.read_sql_query(start_date_query, self.engine)
+            if start_date['Date'][0] is None:
+                data = dr.DataReader(symbol, self.datasource, self.start, self.end)
+                symbol = [symbol] * len(data)
+                data['Symbol'] = symbol
+                data = data.reset_index()
+                data.to_sql('data', self.engine, if_exists='append')
+            else:
+                pass
+
+
+
+
+
+    def get_derivatives(self):
+        ticker_query = 'SELECT symbol, expiration FROM symbols WHERE type in ("FUT","OPT")'
+        symbols = pd.read_sql_query(ticker_query, self.engine)
+        for symbol in symbols['symbol']:
+            for year in range(20, 21):
+                for month in symbols['expiration'][0]:
+                    contract = symbol+month+str(year)+'.CME'
+                    data = dr.DataReader(contract, self.datasource, self.start, self.end)
+                    print(data)
+
 
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from db_connection import DBConnection
-from data_fetch import
+from data_fetch import DataFetch
+
 mysql_conn = DBConnection().mysql_engine()
+
+data_fetch = DataFetch(mysql_conn)
+data_fetch.get_equities()
+#data_fetch.get_derivatives()
