aad06abc5e9b7c1450f36c9a8864032ccebc4fb4
John Gettel <jwgettel@gmail.com>
2020-04-19 22:42:40 -0400

    simulator

--- trade_sim.py
+++ trade_sim.py
@@ -11,20 +11,34 @@ class TradeSimulation:
         symbols = pd.read_sql_query(ticker_query, self.engine)
 
         for symbol in symbols['symbol']:
-            data_query = 'SELECT * FROM trading_signals WHERE Symbol="{}"'.format(symbol)
+            data_query = 'SELECT A.ID, A.Symbol, A.Date, A.Close, B.EMA_Cross FROM data AS A, trading_signals AS B ' \
+                         'WHERE A.ID = B.ID AND A.Symbol="{}"'.format(symbol)
             data = pd.read_sql_query(data_query, self.engine)
 
-            for i in range(len(data)):
-                bank = self.bank
-                if data['EMA_Cross'][i] == 10:
-                    pass
+            data['Position'] = [0] * len(data)
+            data['Shares'] = [0] * len(data)
+            data['Bank'] = [0] * len(data)
+            data.loc[0, 'Bank'] = self.bank
 
+            print(data['Bank'][0])
+            for i in range(1, len(data)):
+                if data['EMA_Cross'][i] == 10 and data['Position'][i-1] == 0 or data['EMA_Cross'][i] == -10 and data['Position'][i-1] == 0:
+                    data['Position'][i] = data['EMA_Cross'][i]
+                    data['Shares'][i] = data['Bank'][i-1] // data['Close'][i]
+                    data['Bank'][i] = data['Bank'][i-1] - (data['Shares'][i] * data['Close'][i])
 
+                elif data['EMA_Cross'][i] == 1 and data['Position'][i-1] == 10 or data['EMA_Cross'][i] == -1 and data['Position'][i-1] == -10:
+                    data['Position'][i] = 0
+                    data['Shares'][i] = 0
+                    data['Bank'][i] = data['Bank'][i - 1] + (data['Shares'][i-1] * data['Close'][i])
 
+                elif data['EMA_Cross'][i] == 10 and data['Position'][i - 1] == -10 or data['EMA_Cross'][i] == -10 and data['Position'][i - 1] == 10:
+                    data['Position'][i] = data['EMA_Cross'][i]
+                    bank = data['Bank'][i - 1] + (data['Shares'][i - 1] * data['Close'][i])
+                    data['Shares'][i] = bank // data['Close'][i]
+                    data['Bank'][i] = bank - (data['Shares'][i] * data['Close'][i])
 
-
-
-
-
-            print(data)
-
+                else:
+                    data.loc[i, 'Position'] = data.loc[i-1, 'Position']
+                    data.loc[i, 'Shares'] = data.loc[i-1, 'Shares']
+                    data.loc[i, 'Bank'] = data.loc[i-1, 'Bank']
